f27cf754da1582ddc94b6da2fe43419a4011fc71
Jose <j.huircaleo01@ufromail.cl>
2022-06-22 04:07:58 -0400

    si

---
+++ src/main/java/Principal.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class Principal {
+    public static void main(String[] args) throws IOException {
+        Rut si = new Rut();
+        si.ValidarRut();
+
+    }
+}
